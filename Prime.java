11a1fecf7f20302fbc0de5848faa2aa65b67eb21
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-08-22 21:32:02 +0530

    import java.util.Scanner; public class odd{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int c = 0;         for(int i = 1;i <= n;i++){             if(n%i == 0){                 c++;             }             }             if(c == 2){                 System.out.print("Prime");             }             else{                 System.out.print("Not Prime");             }         }     }

---
+++ Prime.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class odd{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int c = 0;
+        for(int i = 1;i <= n;i++){
+            if(n%i == 0){
+                c++;
+            }
+            }
+            if(c == 2){
+                System.out.print("Prime");
+            }
+            else{
+                System.out.print("Not Prime");
+            }
+        }
+    }
