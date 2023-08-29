7faed37285b266260e094118fa0d3a020642475c
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-08-29 11:49:54 +0530

    import java.util.*; public class hii {     public static void main(String args[])     {         int x,y,z;         Scanner sc=new Scanner(System.in);         x=sc.nextInt();         y=sc.nextInt();         z=sc.nextInt();         int g=(z-y)/x;         System.out.println(g);
        }
    }

---
+++ Can_cross_the_bridge.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class hii
+{
+    public static void main(String args[])
+    {
+        int x,y,z;
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+        y=sc.nextInt();
+        z=sc.nextInt();
+        int g=(z-y)/x;
+        System.out.println(g);
+        
+        
+    }
+}
