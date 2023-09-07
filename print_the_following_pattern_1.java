6b7147080324cd410ab83ed5e57b0ab8bb5b8e10
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-09-07 11:59:21 +0530

    import java.util.Scanner; public class pattern{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i = 0;i < n;i++){             for(int j = 1;j <= n-i;j++){                 System.out.printf("%d",j);             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class pattern{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0;i < n;i++){
+            for(int j = 1;j <= n-i;j++){
+                System.out.printf("%d",j);
+            }
+            System.out.println();
+        }
+    }
+}
