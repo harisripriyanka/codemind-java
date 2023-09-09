ce2502922bc3fa4f10027624f9bea5fe72b528d2
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-09-09 10:09:11 +0530

    import java.util.Scanner; public class share{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         int y = sc.nextInt();         if(x == 0 && y%2 == 0)         {             System.out.println("YES");         }         else if(x == 0 && y%2 != 0){             System.out.println("NO");         }         else if((x+2*y)%2 == 0){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Can_share_equally?.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class share{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if(x == 0 && y%2 == 0)
+        {
+            System.out.println("YES");
+        }
+        else if(x == 0 && y%2 != 0){
+            System.out.println("NO");
+        }
+        else if((x+2*y)%2 == 0){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
