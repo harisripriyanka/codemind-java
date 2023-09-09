e982be148a2ae48527dd57afec87d750ef793410
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-09-09 19:05:49 +0530

    import java.util.*; public class robber{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int[] arr = new int[n];         for(int i = 0;i < n;i++){             arr[i] = sc.nextInt();         }         int r  = 0;         for(int i = 0;i < n;i++){             if(arr[i]%2 != 0){                 r++;             }         }         if(r <= 2){             System.out.println("YES");         }         else{             System.out.println("NO");         }     } }

---
+++ Can_Robber_Escape?.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class robber{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i = 0;i < n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int r  = 0;
+        for(int i = 0;i < n;i++){
+            if(arr[i]%2 != 0){
+                r++;
+            }
+        }
+        if(r <= 2){
+            System.out.println("YES");
+        }
+        else{
+            System.out.println("NO");
+        }
+    }
+}
