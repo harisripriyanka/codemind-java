9ae6991ab6536947db883f74045359416d1fadae
harisripriyanka <119839623+harisripriyanka@users.noreply.github.com>
2023-09-09 19:16:07 +0530

    import java.util.*; public class conversion{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String s =  sc.nextLine();         int num = Integer.parseInt(s,8);         System.out.println(num);     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class conversion{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String s =  sc.nextLine();
+        int num = Integer.parseInt(s,8);
+        System.out.println(num);
+    }
+}
